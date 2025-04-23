889a594ad65f790225b0e3a77aee1459df151e7c
Derek <dereknugroho@gmail.com>
2025-04-23 14:50:51 -0300

    Begin limit order implementation

--- api_services.py
+++ api_services.py
@@ -5,6 +5,17 @@ from datetime import datetime
 
 import kraken_auth
 
+def asset_pairs():
+    """Retrieve tradable asset pairs"""
+    res_asset_pairs = kraken_auth.kraken_request(
+        '/0/public/AssetPairs',
+        {
+            "nonce": str(int(1000*time.time()))
+        },
+    )
+    # print(f'{datetime.now()} [Kraken API Call] Asset pairs:\n{res_asset_pairs.json()}')
+    return res_asset_pairs.json()
+
 def account_balances():
     """Retrieve account balances for all assets"""
     res_acc_bal = kraken_auth.kraken_request(
@@ -33,7 +44,7 @@ def order_book(pair):
     # print(f'{datetime.now()} [Kraken API Call] Order book ({pair}):\n{res_order_book.json()}')
     return res_order_book.json()
 
-def add_order(order_type, direction, volume, pair):
+def add_order(order_type, direction, volume, pair, price=0):
     """Create a new order"""
     res_add_order = kraken_auth.kraken_request(
         '/0/private/AddOrder',
@@ -41,9 +52,22 @@ def add_order(order_type, direction, volume, pair):
             "nonce": str(int(1000*time.time())),
             "ordertype": order_type,
             "type": direction,
+            "price": price,
             "volume": volume,
             "pair": pair,
         },
     )
     # print(f'{datetime.now()} [Kraken API Call] Trade response:\n{res_add_order.json()}')
     return res_add_order.json()
+
+def open_orders():
+    """Retrieve account balances for all assets"""
+    res_open_orders = kraken_auth.kraken_request(
+        '/0/private/OpenOrders',
+        {
+            "nonce": str(int(1000*time.time()))
+        },
+    )
+    # print(f'{datetime.now()} [Kraken API Call] Open orders:\n{res_open_orders.json()}')
+    print(res_open_orders)
+    return res_open_orders.json()
--- trader.py
+++ trader.py
@@ -1,57 +1,57 @@
 TRADING_FEE = 0.004
 
-# Generate a cleaned list of sorted bid or ask orders
-def clean_order_book(raw_order_book, kraken_pair_id, last_trade_direction, expected_vol):
-    cleaned_order_book = []
-    cumulative_trade_vol = 0.0
+# # Generate a cleaned list of sorted bid or ask orders
+# def clean_order_book(raw_order_book, kraken_pair_id, last_trade_direction, expected_vol):
+#     cleaned_order_book = []
+#     cumulative_trade_vol = 0.0
 
-    # Retrieve bid or ask order book
-    if last_trade_direction == 'buy':
-        raw_order_book = raw_order_book['result'][kraken_pair_id]['bids']
-    elif last_trade_direction == 'sell':
-        raw_order_book = raw_order_book['result'][kraken_pair_id]['asks']
-    else:
-        raise ValueError('last_trade_direction argument must be "buy" or "sell"')
+#     # Retrieve bid or ask order book
+#     if last_trade_direction == 'buy':
+#         raw_order_book = raw_order_book['result'][kraken_pair_id]['bids']
+#     elif last_trade_direction == 'sell':
+#         raw_order_book = raw_order_book['result'][kraken_pair_id]['asks']
+#     else:
+#         raise ValueError('last_trade_direction argument must be "buy" or "sell"')
 
-    # Generate clean order book
-    for raw_order in raw_order_book:
-        cleaned_order = []
-        cleaned_order.append(float(raw_order[0]))
-        cleaned_order.append(float(raw_order[1]))
-        cleaned_order_book.append(cleaned_order)
-        cumulative_trade_vol += float(raw_order[1])
+#     # Generate clean order book
+#     for raw_order in raw_order_book:
+#         cleaned_order = []
+#         cleaned_order.append(float(raw_order[0]))
+#         cleaned_order.append(float(raw_order[1]))
+#         cleaned_order_book.append(cleaned_order)
+#         cumulative_trade_vol += float(raw_order[1])
 
-        if cumulative_trade_vol > expected_vol:
-            break
+#         if cumulative_trade_vol > expected_vol:
+#             break
     
-    return cleaned_order_book
+#     return cleaned_order_book
 
-# Calculate the real-time net cost of the previous trade
-def last_trade_real_time_net_cost(cleaned_order_book, last_trade_vol, last_trade_direction):
-    real_time_cost = 0.0
-    cumulative_order_vol = 0.0
+# # Calculate the real-time net cost of the previous trade
+# def last_trade_real_time_net_cost(cleaned_order_book, last_trade_vol, last_trade_direction):
+#     real_time_cost = 0.0
+#     cumulative_order_vol = 0.0
 
-    for order in cleaned_order_book:
-        cumulative_order_vol += order[1]
-        if cumulative_order_vol <= last_trade_vol:
-            real_time_cost += (order[1] * order[0])
-        else:
-            excess_trade_vol = last_trade_vol - cumulative_order_vol + order[1]
-            real_time_cost += (excess_trade_vol * order[0])
-            break
+#     for order in cleaned_order_book:
+#         cumulative_order_vol += order[1]
+#         if cumulative_order_vol <= last_trade_vol:
+#             real_time_cost += (order[1] * order[0])
+#         else:
+#             excess_trade_vol = last_trade_vol - cumulative_order_vol + order[1]
+#             real_time_cost += (excess_trade_vol * order[0])
+#             break
 
-    if last_trade_direction == 'buy':
-        return real_time_cost * (1 - TRADING_FEE)
-    elif last_trade_direction == 'sell':
-        return real_time_cost * (1 + TRADING_FEE)
-    else:
-        raise ValueError('last_trade_direction argument must be "buy" or "sell"')
+#     if last_trade_direction == 'buy':
+#         return real_time_cost * (1 - TRADING_FEE)
+#     elif last_trade_direction == 'sell':
+#         return real_time_cost * (1 + TRADING_FEE)
+#     else:
+#         raise ValueError('last_trade_direction argument must be "buy" or "sell"')
 
-# Check if the real-time net cost of the last trade crosses the trading threshold
-def crosses_trading_threshold(last_trade_net_cost, last_trade_real_time_net_cost, last_trade_direction, required_return):
-    if last_trade_direction == 'buy':
-        return last_trade_real_time_net_cost >= ((1 + required_return) * last_trade_net_cost)
-    elif last_trade_direction == 'sell':
-        return last_trade_real_time_net_cost <= ((1 - required_return) * last_trade_net_cost)
-    else:
-        raise ValueError('last_trade_direction argument must be "buy" or "sell"')
+# # Check if the real-time net cost of the last trade crosses the trading threshold
+# def crosses_trading_threshold(last_trade_net_cost, last_trade_real_time_net_cost, last_trade_direction, required_return):
+#     if last_trade_direction == 'buy':
+#         return last_trade_real_time_net_cost >= ((1 + required_return) * last_trade_net_cost)
+#     elif last_trade_direction == 'sell':
+#         return last_trade_real_time_net_cost <= ((1 - required_return) * last_trade_net_cost)
+#     else:
+#         raise ValueError('last_trade_direction argument must be "buy" or "sell"')
