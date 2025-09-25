adb2c23bdf1381a237faaaffd6a7ffff40807342
Derek <dereknugroho@gmail.com>
2025-09-25 19:54:20 -0400

    Add layer between low-level authentication and high-level endpoint API calls

---
+++ src/api_client.py
@@ -0,0 +1,18 @@
+import json
+
+from src.config import config, get_kraken_keys
+from src.kraken_auth import request
+
+def make_request(path: str, body: dict = None):
+    """A function for making API requests."""
+    public_key, private_key = get_kraken_keys()
+    response = request(
+        method=config["method"],
+        path=path,
+        body=body,
+        public_key=public_key,
+        private_key=private_key,
+        environment=config["environment"]
+    )
+
+    return json.loads(response.read().decode("utf-8"))
