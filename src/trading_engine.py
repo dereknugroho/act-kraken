2aa3309e9f319dfece1c879131340e95182a5edf
Derek <dereknugroho@gmail.com>
2025-10-02 15:38:40 -0300

    Rename imports, uncomment buy-side trading logic

--- src/trading_engine.py
+++ src/trading_engine.py
@@ -6,12 +6,11 @@ def trade(last_trade: dict) -> None:
     Implement trading strategy and execute trade.
 
     Example:
-    >>> trade(services.last_trade())
+    >>> trade(trading_services.last_trade())
     """
 
     # If there are no open orders, prepare to create a limit order
     if not kraken_services.open_orders()["result"]["open"]:
-
         # Calculate trading fee
         fee = trading_services.trading_fee(
             pair="BTC/USD",
@@ -41,21 +40,23 @@ def trade(last_trade: dict) -> None:
                 price=str(limit_price),
             )
 
-        # # If the last trade was a sell:
-        # elif last_trade["type"] == "sell":
+        # If the last trade was a sell:
+        elif last_trade["type"] == "sell":
 
-        #     # Calculate maximum purchase volume
-        #     max_buy_vol = trading_services.maximum_buy_volume(
-        #         account_balances=kraken_services.account_balances(),
-        #         price=limit_price,
-        #         precision=5,
-        #     )
+            # Calculate maximum purchase volume
+            max_buy_vol = trading_services.maximum_buy_volume(
+                account_balances=kraken_services.account_balances(),
+                price=limit_price,
+                precision=5,
+            )
 
-        #     # Create a limit buy order
-        #     new_order = kraken_services.add_order(
-        #         ordertype="limit",
-        #         type="buy",
-        #         volume=max_buy_vol,
-        #         pair="BTC/USD",
-        #         price=str(limit_price),
-        #     )
+            # Create a limit buy order
+            new_order = kraken_services.add_order(
+                ordertype="limit",
+                type="buy",
+                volume=max_buy_vol,
+                pair="BTC/USD",
+                price=str(limit_price),
+            )
+    else:
+        pass
