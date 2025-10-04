b055a8e489e3f0408359157aee89cf6ed7ba55fa
Derek <dereknugroho@gmail.com>
2025-10-04 10:54:21 -0300

    Implement logging in Kraken API request wrapper

--- src/kraken_client.py
+++ src/kraken_client.py
@@ -6,13 +6,25 @@ from src.kraken_auth import request
 def make_request(path: str, body: dict = None):
     """A wrapper for making API requests."""
     public_key, private_key = get_kraken_keys()
-    response = request(
-        method=config["method"],
-        path=path,
-        body=body,
-        public_key=public_key,
-        private_key=private_key,
-        environment=config["environment"]
-    )
 
-    return json.loads(response.read().decode("utf-8"))
+    try:
+        response = request(
+            method=config["method"],
+            path=path,
+            body=body,
+            public_key=public_key,
+            private_key=private_key,
+            environment=config["environment"]
+        )
+        data = json.loads(response.read().decode("utf-8"))
+
+        # Handle Kraken's internal error response
+        if data['error']:
+            logger.error(f'Kraken API returned error(s): {data["error"]}')
+            return None
+
+        return data
+
+    except Exception as e:
+        logger.exception(f'Request to Kraken failed for path {path}: {e}')
+        raise
